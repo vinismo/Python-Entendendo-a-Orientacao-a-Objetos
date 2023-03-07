5ed59591c500d6320e0e06e00ab9973a4da27b93
Vinicius Ducatti Ferraz <110203525+vinismo@users.noreply.github.com>
2023-03-07 08:24:16 -0300

    Add files via upload

--- Python Entendendo a Orientação a Objetos/oo/teste.py
+++ Python Entendendo a Orientação a Objetos/oo/teste.py
@@ -2,3 +2,12 @@
 def cria_conta(numero, titular, saldo, limite):
     conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
     return conta
+
+def deposita(conta, valor):
+    conta["saldo"] += valor
+
+def saca(conta, valor):
+    conta["saldo"] -= valor
+
+def extrato(conta):
+    print("Saldo: R${}".format(conta["saldo"]))
